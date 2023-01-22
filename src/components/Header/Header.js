3ed1eb2e802b908baa875368995b3b0bf697b62c
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-01-22 20:47:50 +0000

    Add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,26 @@ import styles from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={styles[`header`]}>
             <section>
+                <div className={styles.logo}> <img src="images/logo.svg" alt="logo" /></div>
+                <div></div>
+                <div></div>
             </section>
-
             <nav >
+                    <ul>
+                        <li><a className={styles.navBtn}>All</a></li>
+                        <li><a className={styles.navBtn}>Offers</a></li>
+                        <li><a className={styles.navBtn}>Chinese</a></li>
+                        <li><a className={styles.navBtn}>Pizza</a></li>
+                        <li><a className={styles.navBtn}>Burgers</a></li>
+                        <li><a className={styles.navBtn}>Doners</a></li>
+                        <li><a className={styles.navBtn}>Sushi</a></li>
+                        <li><a className={styles.navBtn}>Mexican</a></li>
+                        <li><a className={styles.navBtn}>Vegeterian</a></li>
+                        <li><a className={styles.navBtn}>Indian</a></li>
+
+                    </ul>  
             </nav>
         </header>
     );
