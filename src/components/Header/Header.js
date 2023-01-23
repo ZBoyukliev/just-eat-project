10b1666a9c07beea90b58c299aae244bdb5813ab
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-01-23 13:23:25 +0000

    Change the language on header nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,21 +5,23 @@ const Header = () => {
         <header className={styles[`header`]}>
             <section>
                 <div className={styles.logo}> <img src="images/logo.svg" alt="logo" /></div>
-                <div></div>
-                <div></div>
+                <div className={styles.inputField}>
+                    <input placeholder='Адрес, 111 Bulgaria Blvd. '></input>
+                </div>
+                <div className={styles.navField}></div>
             </section>
             <nav >
                     <ul>
-                        <li><a className={styles.navBtn}>All</a></li>
-                        <li><a className={styles.navBtn}>Offers</a></li>
-                        <li><a className={styles.navBtn}>Chinese</a></li>
-                        <li><a className={styles.navBtn}>Pizza</a></li>
-                        <li><a className={styles.navBtn}>Burgers</a></li>
-                        <li><a className={styles.navBtn}>Doners</a></li>
-                        <li><a className={styles.navBtn}>Sushi</a></li>
-                        <li><a className={styles.navBtn}>Mexican</a></li>
-                        <li><a className={styles.navBtn}>Vegeterian</a></li>
-                        <li><a className={styles.navBtn}>Indian</a></li>
+                        <li><a className={styles.navBtn}>Всички</a></li>
+                        <li><a className={styles.navBtn}>Оферти</a></li>
+                        <li><a className={styles.navBtn}>Китайска</a></li>
+                        <li><a className={styles.navBtn}>Пици</a></li>
+                        <li><a className={styles.navBtn}>Бургери</a></li>
+                        <li><a className={styles.navBtn}>Дюнери</a></li>
+                        <li><a className={styles.navBtn}>Суши</a></li>
+                        <li><a className={styles.navBtn}>Мексиканска</a></li>
+                        <li><a className={styles.navBtn}>Вегетарианска</a></li>
+                        <li><a className={styles.navBtn}>Индийска</a></li>
 
                     </ul>  
             </nav>
