adcff8da62959cbb876527058029c420b9e0e511
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-01-25 20:50:47 +0000

    Update home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,20 +2,35 @@ import styles from './Home.module.css'
 
 const Home = () => {
     return (
-            <section className={styles.findRestaurant}>
-                <div className={styles.leftSide}>
-                    <div className={styles.search}>
-                        <h1>Време е да поръчаш нещо вкусно</h1>
-                        <h2>Открий ресторантите в твоя район</h2>
-                        <input placeholder='Адрес, 111 Bulgaria Blvd. '></input>
-                    </div>
+        <>
+        <section className={styles.findRestaurant}>
+            <div className={styles.leftSide}>
+                <div className={styles.search}>
+                    <h1>Време е да поръчаш нещо вкусно</h1>
+                    <h2>Открий ресторантите в твоя район</h2>
+                    <input placeholder='Адрес, 111 Bulgaria Blvd. '></input>
                 </div>
-                <div className={styles.rightSide}>
-                    <div className={styles.image}>
-                        <img src='images/doner.png' alt='doner'></img>
-                    </div>
+            </div>
+            <div className={styles.rightSide}>
+
+                <div className={styles.image}>
+                    <img src='images/doner.png' alt='doner'></img>
                 </div>
-            </section>
+                <svg>
+                </svg>
+            </div>
+        </section>
+        <section className={styles.basicInfo}>
+            <div>
+                <h3>
+                Как да поръчаш?
+                </h3>
+                <h2>
+                Толкова е лесно.
+                </h2>
+            </div>
+        </section>
+        </>
     );
 };
 
