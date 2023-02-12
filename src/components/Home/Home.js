609d5c4f6923a3a8b89096ea5b0cf049952f9207
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-02-12 18:36:01 +0000

    Add footer to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -61,17 +61,24 @@ const Home = () => {
                 </section>
             </section>
             <section className={styles[`links-to-apps`]}>
-                    <div className={styles[`left-side`]}>
-                        <h2>Изтегли приложението</h2>
-                        <h3>Избери ресторант, поръчай и хапни!</h3>
-                        <img src='/images/appstore.png' alt='appstor'></img>
-                        <img src='/images/googleplay.png' alt='googleplay'></img>
-                        <img src='/images/appgallery.png' alt='appgallery'></img>
-                    </div>
-                    <div className={styles[`right-side`]}>
+                <div className={styles[`left-side`]}>
+                    <h2>Изтегли приложението</h2>
+                    <h3>Избери ресторант, поръчай и хапни!</h3>
+                    <img src='/images/appstore.png' alt='appstor'></img>
+                    <img src='/images/googleplay.png' alt='googleplay'></img>
+                    <img src='/images/appgallery.png' alt='appgallery'></img>
+                </div>
+                <div className={styles[`right-side`]}>
                     <img src='/images/mobile.png' alt='mobile'></img>
-                    </div>
+                </div>
             </section>
+            <footer>
+                <p>Follow us</p>
+                <a href='https://www.facebook.com/timefortakeaway' title='facebook'><i class="fa-brands fa-facebook"></i></a>
+                <a href='https://www.instagram.com/takeawaycom_bg'><i class="fa-brands fa-instagram"></i></a>
+                <a href='https://twitter.com/TakeawaycomBG'><i class="fa-brands fa-twitter"></i></a>
+                <a href='https://www.youtube.com/channel/UCM1QOg3YfCmfszGXvwErHPw'><i class="fa-brands fa-youtube"></i></a>
+            </footer>
         </>
     );
 };
