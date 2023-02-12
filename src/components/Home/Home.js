2fb745a0d3043481ed523875c021e0c9a735e6de
ZBoyukliev <zarkoboyukliev@gmail.com>
2023-02-12 15:07:43 +0000

    Add section to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -61,7 +61,16 @@ const Home = () => {
                 </section>
             </section>
             <section className={styles[`links-to-apps`]}>
-                    Hello
+                    <div className={styles[`left-side`]}>
+                        <h2>Изтегли приложението</h2>
+                        <h3>Избери ресторант, поръчай и хапни!</h3>
+                        <img src='/images/appstore.png' alt='appstor'></img>
+                        <img src='/images/googleplay.png' alt='googleplay'></img>
+                        <img src='/images/appgallery.png' alt='appgallery'></img>
+                    </div>
+                    <div className={styles[`right-side`]}>
+                    <img src='/images/mobile.png' alt='mobile'></img>
+                    </div>
             </section>
         </>
     );
